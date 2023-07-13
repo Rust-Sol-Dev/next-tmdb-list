bc71197963a0b08d95ce0d50874fe67be2fc3770
mrrust77 <felixmartel.no@gmail.com>
2023-07-14 04:30:03 +0900

    update: pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 export interface PaginationProps {
@@ -32,19 +31,59 @@ const Pagination: React.FC<PaginationProps> = ({
               <FaChevronLeft size={16} />
             </button>
           </li>
-          {/* {Array.from({ length: totalPages }, (_, index) => index + 1).map(
-            (page) => (
-              <li key={page}>
-                <button
-                  className="grid w-8 h-8 border rounded-sm place-content-center"
-                  disabled={currentPage === page}
-                  onClick={() => handlePageChange(page)}
-                >
-                  {page}
-                </button>
-              </li>
-            )
-          )} */}
+          {currentPage > 2 && (
+            <li>
+              <button
+                className="grid w-8 h-8 border rounded-sm place-content-center"
+                onClick={() => handlePageChange(1)}
+              >
+                1
+              </button>
+            </li>
+          )}
+          {currentPage > 3 && (
+            <li>
+              <span className="px-2">...</span>
+            </li>
+          )}
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (page) => {
+              if (
+                page === currentPage ||
+                (page === currentPage - 1 && currentPage === 2) ||
+                (page === currentPage + 1 && currentPage === totalPages - 1) ||
+                (page > currentPage - 2 && page < currentPage + 2)
+              ) {
+                return (
+                  <li key={page}>
+                    <button
+                      className="grid w-8 h-8 border rounded-sm place-content-center"
+                      disabled={currentPage === page}
+                      onClick={() => handlePageChange(page)}
+                    >
+                      {page}
+                    </button>
+                  </li>
+                );
+              }
+              return null;
+            }
+          )}
+          {currentPage < totalPages - 2 && (
+            <li>
+              <span className="px-2">...</span>
+            </li>
+          )}
+          {currentPage < totalPages - 1 && (
+            <li>
+              <button
+                className="grid h-8 border rounded-sm min-w-[32px] place-content-center"
+                onClick={() => handlePageChange(totalPages)}
+              >
+                {totalPages}
+              </button>
+            </li>
+          )}
           <li>
             <button
               className="grid w-8 h-8 border rounded-sm place-content-center"
