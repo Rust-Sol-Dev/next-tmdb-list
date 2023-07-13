4857dd570ff929ebe980ac32bb594db5b5ead505
mrrust77 <felixmartel.no@gmail.com>
2023-07-14 04:08:11 +0900

    update:mobile responsive of detail page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,16 @@ import { FC } from "react";
 const Header: FC = () => {
   return (
     <header className="top-0 left-0 w-full fixed z-50 bg-[#0d253f]">
-      <div className="container px-6 py-4 mx-auto">
+      <div className="container px-2 py-4 mx-auto lg:px-6">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <Link href="/" passHref>
-              <div className="text-3xl font-black text-white">LOGO</div>
+              <div className="text-2xl font-black text-white lg:text-3xl">
+                LOGO
+              </div>
             </Link>
             <nav className="ml-6 text-white">
-              <ul className="flex items-center gap-8">
+              <ul className="flex items-center gap-4 lg:gap-8">
                 {LINKS.map((item, key) => (
                   <li key={key}>
                     <Link
--- src/pages/movie/[movie_id].tsx
+++ src/pages/movie/[movie_id].tsx
@@ -65,42 +65,48 @@ export default function MovieDetail() {
           >
             <div className="absolute top-0 left-0 w-full h-full bg-[#000] opacity-80" />
             <div className="container relative z-10 py-10 mx-auto">
-              <div className="flex gap-6">
+              <div className="flex gap-4 px-6 xl:gap-6">
                 <img
                   src={`https://image.tmdb.org/t/p/w342${data.detail.poster_path}`}
+                  className="hidden lg:block"
                   alt=""
                 />
-                <div className="w-[calc-370px] text-white px-10">
-                  <h1 className="text-[36px] font-bold">{data.detail.title}</h1>
-                  <div className="flex items-center gap-1 mt-2">
+                <div className="w-[calc-370px] text-white px-2 lg:px-10">
+                  <h1 className="text-3xl lg:text-[36px] font-bold">
+                    {data.detail.title}
+                  </h1>
+                  <div className="flex flex-wrap items-center gap-1 mt-2 text-sm lg:text-md">
                     <Link href={data.detail.homepage} passHref target="_blank">
-                      <AiOutlineGlobal size={30} className="mr-2" />
+                      <AiOutlineGlobal size={24} className="mr-2" />
                     </Link>
                     <FaCalendar />
                     {data.detail.release_date}
                     {data.detail.genres.map((item, key) => (
-                      <p key={item.name + key}>, {item.name}</p>
+                      <p key={item.name + key} className="">
+                        {item.name}
+                        {key !== data.detail.genres.length - 1 && ", "}
+                      </p>
                     ))}
                   </div>
                   <div className="my-5">
-                    <h5 className="text-lg font-bold">Overview</h5>
-                    <p>{data.detail.overview}</p>
+                    <h5 className="font-bold text-md lg:text-lg">Overview</h5>
+                    <p className="text-sm lg:text-md">{data.detail.overview}</p>
                   </div>
-                  <p className="my-1 text-lg capitalize">
+                  <p className="my-1 text-sm capitalize lg:text-lg">
                     status:{" "}
                     <span className="font-bold">{data.detail.status}</span>
                   </p>
-                  <p className="my-1 text-lg capitalize">
+                  <p className="my-1 text-sm capitalize lg:text-lg">
                     popularity:{" "}
                     <span className="font-bold">{data.detail.popularity}</span>
                   </p>
-                  <p className="my-1 text-lg capitalize">
+                  <p className="my-1 text-sm capitalize lg:text-lg">
                     vote average:{" "}
                     <span className="font-bold">
                       {data.detail.vote_average}
                     </span>
                   </p>
-                  <div className="flex flex-wrap items-center gap-1 mt-2">
+                  <div className="flex flex-wrap items-center gap-1 my-1 mt-2 text-sm capitalize lg:text-lg">
                     Production countries:
                     {data.detail.production_countries.map((item, key) => (
                       <p key={item.name + key} className="font-bold">
@@ -110,7 +116,7 @@ export default function MovieDetail() {
                       </p>
                     ))}
                   </div>
-                  <div className="flex flex-wrap items-center gap-1 mt-2">
+                  <div className="flex flex-wrap items-center gap-1 my-1 mt-2 text-sm capitalize lg:text-lg">
                     Production companies:
                     {data.detail.production_companies.map((item, key) => (
                       <p key={item.name + key} className="font-bold">
@@ -125,13 +131,13 @@ export default function MovieDetail() {
             </div>
           </div>
           {data && data.credits && (
-            <div className="container mx-auto max-w-[1280px] pt-10 pb-20">
-              <div className="grid grid-cols-2 gap-10">
+            <div className="container mx-auto max-w-[1280px] pt-10 pb-20 px-4 lg:px-0">
+              <div className="grid grid-cols-1 gap-10 sm:grid-cols-2">
                 <div className="">
                   <h2 className="mb-2 text-2xl font-bold">
                     Cast ({data.credits.cast.length})
                   </h2>
-                  <div className="grid grid-cols-5 gap-3">
+                  <div className="grid grid-cols-4 gap-3 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                     {data.credits.cast.map((cast, key) => (
                       <CastOne key={key} {...cast} />
                     ))}
@@ -141,7 +147,7 @@ export default function MovieDetail() {
                   <h2 className="mb-2 text-2xl font-bold">
                     Crew ({data.credits.crew.length})
                   </h2>
-                  <div className="grid grid-cols-5 gap-3">
+                  <div className="grid grid-cols-4 gap-3 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                     {data.credits.crew.map((crew, key) => (
                       <CrewOne key={key} {...crew} />
                     ))}
